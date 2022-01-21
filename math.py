d401d90cc30e4c90c6a5ccd2eaae6a78a2e7ea2c
onkiieee <98167361+onkiieee@users.noreply.github.com>
2022-01-21 19:38:27 +0530

    Python File Commit

---
+++ math.py
@@ -0,0 +1,16 @@
+#Add Implementation
+def add(x,y):
+	return x+y
+
+#Sub Implementation
+def sub(x,y):
+	return x-y
+#Mult Implementation
+def mult(x,y):
+	x*y
+
+#Div Implementation
+def div(x,y)
+	return x/y
+
+#onkar Added in bug123
